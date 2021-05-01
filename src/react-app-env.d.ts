bc6fae4fb36a60f3cad8b22acf32a8e69e150aca
vikbert <segentor@gmail.com>
2021-05-01 14:57:55 +0200

    optimize the build

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -9,12 +9,12 @@ declare namespace NodeJS {
   interface ProcessEnv {
     /**
      * By default, there are two modes in Vite:
-     * 
+     *
      * * `development` is used by vite and vite serve
      * * `production` is used by vite build
-     * 
+     *
      * You can overwrite the default mode used for a command by passing the --mode option flag.
-     * 
+     *
      */
     readonly NODE_ENV: 'development' | 'production'
   }
@@ -72,3 +72,8 @@ declare module '*.module.sass' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+/**
+ * my declaration
+ */
+ declare module 'prismjs/components/prism-core'
