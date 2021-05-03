287fbe5651c266a1a4cdbaa11d2e2720322ed352
vikbert <segentor@gmail.com>
2021-05-03 11:31:12 +0200

    router works

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,36 @@
 import React from 'react';
 import {Provider as ReduxProvider} from 'react-redux';
+import {Route} from 'wouter';
 import './app.css';
 import './utilities.css';
-import useLocalStorage from "./hooks/useLocalStorage";
-import Bad from "./pages/Bad";
 import Good from "./pages/Good";
 import Home from "./pages/Home";
 import Insert from "./pages/Insert";
 import Navigation from "./pages/Navigation";
 import Play from "./pages/Play";
 
-const STORAGE_KEY = 'RATE_CODE';
-const STORAGE_DATA = {
-  currentPage: 'insert',
-}
-
 // @ts-ignore
 import configureStore from './redux/configureStore';
-
 const store = configureStore();
 
 const App = () => {
-  const [storedValue, setValue] = useLocalStorage(STORAGE_KEY, STORAGE_DATA)
-
-  // @ts-ignore
-  const {currentPage} = storedValue;
-  console.log(storedValue);
-
   return (
       <>
         <ReduxProvider store={store}>
-          <div className="ct" id="home">
-            <div className="ct" id="play">
-              <div className="ct" id="insert">
-                <div className="ct" id="good">
-                  <div className="ct" id="bad">
-                    <Navigation/>
-                    <Home/>
-                    <Play/>
-                    <Insert/>
-                    <Good/>
-                    <Bad/>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="main">
+            <Route path={'/'}>
+              <Home/>
+            </Route>
+            <Route path={'/play'}>
+              <Play/>
+            </Route>
+            <Route path={'/insert'}>
+              <Insert/>
+            </Route>
+            <Route path={'/good'}>
+              <Good/>
+            </Route>
+            <Navigation/>
           </div>
         </ReduxProvider>
       </>
--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
+import {Link} from 'wouter'
 
 const Navigation = () => {
   return (
       <ul id="menu">
-        <a href="#home">
+        <Link href="/">
           <li className="icon" id="uno">
             <span className="iconify" data-icon="fluent:home-12-regular" data-inline="false"/>
           </li>
-        </a>
-        <a href="#play">
+        </Link>
+        <Link href="/play">
           <li className="icon" id="dos">
             <span className="iconify" data-icon="ant-design:play-circle-outlined" data-inline="false"/>
           </li>
-        </a>
-        <a href="#insert">
+        </Link>
+        <Link href="/insert">
           <li className="icon" id="tres">
             <span className="iconify" data-icon="carbon:add-alt" data-inline="false"/>
           </li>
-        </a>
-        <a href="#good">
+        </Link>
+        <Link href="/good">
           <li className="icon" id="cuatro">
             <span className="iconify" data-icon="ic:outline-thumb-up-off-alt" data-inline="false"/>
           </li>
-        </a>
-        <a href="#bad">
-          <li className="icon" id="cinco">
-            <span className="iconify" data-icon="ic:outline-thumb-down-off-alt" data-inline="false"/>
-          </li>
-        </a>
+        </Link>
       </ul>
   );
 };
