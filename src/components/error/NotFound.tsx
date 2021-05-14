c80584441c9c4edb51cac7c3520cfc72cba74b82
vikbert <segentor@gmail.com>
2021-05-14 15:40:19 +0200

    add not found component

---
+++ src/components/error/NotFound.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function (): JSX.Element {
+  return (
+    <>
+      <div className="grid ai-center jc-center gs32 md:fd-column">
+        <div className="grid--cell sm:d-none">
+          <img className="wmx100" src="https://cdn.sstatic.net/Sites/stackoverflow/img/404.svg" alt="Page not found" />
+        </div>
+        <div className="grid--cell wmx5">
+          <h1 className="fs-headline1 mb4">Ops</h1>
+          <p>Page not found !</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
