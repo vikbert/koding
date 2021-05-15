00a0413a40accef5e3b39b35216a52c643e42f95
vikbert <segentor@gmail.com>
2021-05-15 23:07:15 +0200

    updates

--- src/components/link/ActionContainer.tsx
+++ src/components/link/ActionContainer.tsx
@@ -7,14 +7,14 @@ type PropsT = {
 export default function ActionContainer(props: PropsT): JSX.Element {
   return (
     <div className="grid--cell mr16 fl1 w96">
-        <div className="grid d-flex gs8 s-anchors s-anchors__muted fw-wrap">
-          {props.children.map((item: any, index: number) => (
-            <div className="grid--cell" key={index}>
-              {item}
-            </div>
-          ))}
-        </div>
-        <div className="js-menu-popup-container" />
+      <div className="grid d-flex gs8 s-anchors s-anchors__muted fw-wrap">
+        {props.children.map((item: any, index: number) => (
+          <div className="grid--cell" key={index}>
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="js-menu-popup-container" />
     </div>
   );
 }
