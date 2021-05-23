e892b6d77eaf8bba3168d8f4504357f4b6d55dd7
vikbert <segentor@gmail.com>
2021-05-23 13:15:16 +0200

    updates

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,17 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import rootReducer, {rootState} from './storeReducer';
 import thunk from 'redux-thunk';
+import loggerMiddleware from './middleware/logger';
+import monitorReducerEnhancer from './middleware/monitorReducer';
 
 export default function configureStore() {
-  let composerEnhancers = () => {};
-  if (process.env.NODE_ENV === 'development') {
-    composerEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  }
+  // let composerEnhancers = () => {};
+  // if (process.env.NODE_ENV === 'development') {
+  //   composerEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  // }
 
-  return createStore(
-    rootReducer,
-    rootState,
-    composerEnhancers(applyMiddleware(thunk)),
-  );
+  const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunk);
+  const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer);
+
+  return createStore(rootReducer, rootState, composedEnhancers);
 }
