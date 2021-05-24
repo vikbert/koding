0e88123a498e627a7613acfc83ce505df1e3189a
vikbert <segentor@gmail.com>
2021-05-24 09:13:28 +0200

    updates

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -2,9 +2,9 @@ import Auth from 'firebase-auth-lite';
 import {Database} from 'firebase-firestore-lite';
 
 const useFirebase = (): Database => {
-  const auth = new Auth({apiKey: 'AIzaSyAsfZp6lD_7HYzU7RNb-ZCUzbsKdhQQtTo'});
+  const auth = new Auth({apiKey: process.env.REACT_APP_FIREBASE_API_KEY});
 
-  return new Database({projectId: 'api-project-984120382986', auth});
+  return new Database({projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '', auth});
 };
 
 export default useFirebase;
