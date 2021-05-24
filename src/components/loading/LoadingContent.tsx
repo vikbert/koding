7f4220bbfc3dab7b8b3bbc6219bf52af364daa42
vikbert <segentor@gmail.com>
2021-05-24 11:43:43 +0200

    show no content after 3 seconds

--- src/components/loading/LoadingContent.tsx
+++ src/components/loading/LoadingContent.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './loading.css';
+import NoContent from '../error/NoContent';
 
 export default function LoadingContent(): JSX.Element {
+  const [showNoData, setShowNoData] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShowNoData(true);
+    }, 3000);
+  }, []);
+
+  if (showNoData) {
+    return <NoContent/>;
+  }
+
   return (
-    <div className="grid ai-center jc-center gs32 md:fd-column mt128">
-      <div id="wrap">
-        <div className="item">
-          <div className="spinner" />
-          <h5>
-            Loading<span className="dot">.</span>
-          </h5>
+      <div className="grid ai-center jc-center gs32 md:fd-column mt128">
+        <div id="wrap">
+          <div className="item">
+            <div className="spinner"/>
+            <h5>
+              Loading<span className="dot">.</span>
+            </h5>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
